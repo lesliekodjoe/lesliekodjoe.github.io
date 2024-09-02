83dbb370c443da3c2b665bd2e0b041054ac3fb5a
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-09-02 13:42:40 +0000

    Header responsive feature

--- src/components/AnimatedHamburgerMenu.tsx
+++ src/components/AnimatedHamburgerMenu.tsx
@@ -1,17 +1,68 @@
 // "use client"
-import { motion } from "framer-motion"
+import { motion, MotionConfig } from "framer-motion"
+import { useState } from "react";
 
 export const AnimatedHamburgerMenu = () => {
+    const [active, setActive] = useState(false);
     return(
-        <motion.button className="relative border h-20 w-20">
-            <motion.span 
-            className="absolute h-1 w-10 bg-black"
-            
+        <MotionConfig
+                transition={{
+            duration: 0.5,
+            ease: "easeInOut"
+        }}
+        >
+            <motion.button
+            initial={false}
+            onClick={() => setActive ((pv)=> !pv)}
+            className="relative border h-10 w-20"
+            animate = {active ? "open" : "closed"}
             >
+                <motion.span 
+                className="absolute h-1 w-10 bg-black rounded-full"
+                style={{
+                    left: "50%",
+                    top: "30%",
+                    x: "-50%",
+                    y: "-50%",
+                    
+                }}
+                variants={{
+                    open: {
+                        rotate: ["0deg", "0deg", "45deg"],
+                        top: ["30%" , "50%", "50%"]
+                        
+                    },
+                    closed: {
+                        rotate: ["45deg", "0deg", "0deg"],
+                        top: [ "50%", "50%", "30%"]
+                    },
+                }}
+                >
 
-            </motion.span>
-            <motion.span></motion.span>
-            <motion.span></motion.span>
-        </motion.button>
+                </motion.span>
+                <motion.span
+                className="absolute h-1 w-10 bg-black rounded-full"
+                style={{
+                    left: "50%",
+                    bottom: "30%",
+                    x: "-50%",
+                    y: "-50%",
+                }}
+
+                variants={{
+                    open: {
+                        rotate: ["0deg", "0deg", "-45deg"],
+                        bottom: ["30%" , "50%", "40%"]
+                        
+                    },
+                    closed: {
+                        rotate: ["-45deg", "0deg", "0deg"],
+                        bottom: [ "40%", "50%", "30%"]
+                    },
+                }}
+
+                ></motion.span>
+            </motion.button>
+        </MotionConfig>
     );
 }
