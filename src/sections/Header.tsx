05380836b376d89b00c66c4738317177fa98ac7a
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-09-03 02:42:59 +0000

    Header responsive feature

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -9,7 +9,7 @@ type Props = {}
 export const Header = () => {
     return (
         <header className='sticky top-0'>
-            <div className='flex justify-between px-8 py-3 border'>
+            <div className='flex items-center justify-between px-8 py-3 border'>
                 <Image src={Logo} alt='logo' height={30} width={30} />
                 <nav className='hidden md:block'>
                     <a href='' className=''>Leslie Kodjoe</a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens: {
-      sm: "375px",
-      md: "768px",
-      lg: "1200px",
-    },
-    extend: {
-      container: {
-        center: true,
-      },
-    },
+  	screens: {
+  		sm: '375px',
+  		md: '768px',
+  		lg: '1200px'
+  	},
+  	extend: {
+  		container: {
+  			center: true
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 
 export default config;
