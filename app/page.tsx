5efd4047769dddf464ff4c0d286931e19eb92cd1
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-09-26 18:02:35 +0000

    Fix menu overlap

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import coneImage from "@/assets/icons/Cone.svg";
 import donutImage from "@/assets/icons/Donut.svg";
 import watchImage from "@/assets/icons/Apple_watches.svg";
@@ -7,10 +8,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
+    <section className="">
       <div className="pt-12 pb-24 lg:pb-44 md:pt-28">
         <div className="container">
-          <div className="relative flex justify-center items-center">
+          <div className="-z-10 relative flex justify-center items-center">
             <div className="w-[309px] md:w-[788px] h-auto">
               <h1 className="text-center text-2xl font-bold text-design md:text-6xl md:leading-tight">
                 A Frontend Developer who loves designs & creating memorable user
@@ -40,20 +41,21 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-[#1D1D1D] text-white">
-        <div className="relative flex items-center justify-center">
+      <div className="relative -z-10 bg-[#1D1D1D] text-white">
+        <div className="flex items-center justify-center">
           <Image
             src={pencilImage}
             alt="Pencil Image"
             className="absolute -top-24"
           />
         </div>
-        <div className="w-full uppercase flex flex-col items-center justify-center gap-y-6 py-36 border div">
+        <div className="w-full uppercase flex flex-col items-center justify-center gap-y-6 py-36 div">
           <span className="text-sm tracking-super-wide bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DDF,#DD7DDF)] text-transparent bg-clip-text">
             Featured
           </span>
           <h1 className="font-extrabold text-5xl">Projects</h1>
         </div>
+        <div></div>
       </div>
     </section>
   );
