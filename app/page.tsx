1330e59c659e0fa24cd9aa66db15d4ce9a2963da
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-09-26 00:01:26 +0000

    Featured Project intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import coneImage from "@/assets/icons/Cone.svg";
+import donutImage from "@/assets/icons/Donut.svg";
+import watchImage from "@/assets/icons/Apple_watches.svg";
+import mouseImage from "@/assets/icons/Mouse.svg";
+import pencilImage from "@/assets/icons/Pencil.svg";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="pt-12 md:pt-28">
+    <div className="pt-12 pb-28 md:pt-28">
       <div className="container">
-        <div className="border relative md:flex justify-center items-center">
+        <div className="relative md:flex justify-center items-center">
           <div className="w-[309px] md:w-[788px] h-auto">
             <h1 className="text-center text-2xl font-bold text-design md:text-6xl md:leading-tight">
               A Frontend Developer who loves designs & creating memorable user
@@ -15,10 +19,33 @@ export default function Home() {
           <Image
             src={coneImage}
             alt="Cone Image"
-            className="hidden md:block w-[210px] absolute -top-20 -right-20"
+            className="hidden lg:block w-[230px] absolute -top-20 -right-28"
+          />
+          <Image
+            src={donutImage}
+            alt="Donut Image"
+            className="hidden lg:block w-[230px] absolute -bottom-28 left-0"
+          />
+          <Image
+            src={watchImage}
+            alt="Watch Image"
+            className="hidden lg:block w-[100px] absolute -top-10 -left-20"
+          />
+          <Image
+            src={mouseImage}
+            alt="Mouse Image"
+            className="hidden lg:block w-[100px] absolute -bottom-10 -right-0"
           />
         </div>
       </div>
-    </section>
+      <div className="mt-14 w-full flex items-center justify-center">
+        <div>
+          <Image src={pencilImage} alt="Pencil Image" />
+        </div>
+      </div>
+      <div>
+        Hello
+      </div>
+    </div>
   );
 }
