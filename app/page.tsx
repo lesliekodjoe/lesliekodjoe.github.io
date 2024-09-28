e66b1a2bd10653142091e0cec7123c729f8f19c7
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-09-28 17:34:40 +0000

    Landing page animations

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import watchImage from "@/assets/icons/Apple_watches.svg";
 import mouseImage from "@/assets/icons/Mouse.svg";
 import pencilImage from "@/assets/icons/Pencil.svg";
 import Image from "next/image";
+import { useRef } from "react";
 
 import proIMG1 from "@/assets/projectBackground/proIMG1.jpg";
 import proIMG2 from "@/assets/projectBackground/proIMG2.jpg";
@@ -12,6 +13,7 @@ import proIMG3 from "@/assets/projectBackground/proIMG3.jpg";
 import proIMG4 from "@/assets/projectBackground/proIMG4.jpg";
 import Connect from "@/sections/Connect";
 import Footer from "@/components/Footer";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const projects_data = [
   {
@@ -41,29 +43,43 @@ const projects_data = [
 ];
 
 export default function Home() {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [120, -400]);
+
   return (
-    <section className="">
+    <section ref={heroRef} className="">
       {/* Hero Section */}
-      <div className="pt-20 pb-36 lg:pb-44 md:pt-28 overflow-x-clip">
+      <div className="pt-20 pb-36 lg:pb-32 md:pt-28 overflow-x-clip">
         <div className="container">
           <div className="-z-10 relative flex justify-center items-center">
-            <div className="w-[309px] md:w-[788px] h-auto">
+            <div className="w-[309px] md:w-[788px] h-auto flex flex-col items-center justify-center">
               <h1 className="text-center text-2xl font-bold text-design md:text-6xl md:leading-tight">
                 A Frontend Developer who loves designs & creating memorable user
                 experiences.
               </h1>
+              <div className="tag">About Me</div>
             </div>
-            <Image
-              src={coneImage}
+            <motion.img
+              src={coneImage.src}
               alt="Cone Image"
               className="w-[150px] lg:block lg:w-[230px] absolute -top-20 -right-28"
               loading="lazy"
+              style={{
+                translateY: translateY,
+              }}
             />
-            <Image
-              src={donutImage}
+            <motion.img
+              src={donutImage.src}
               alt="Donut Image"
               className="w-[150px] -left-10 lg:block lg:w-[230px] absolute -bottom-28 lg:left-0"
               loading="lazy"
+              style={{
+                translateY: translateY,
+              }}
             />
             <Image
               src={watchImage}
@@ -71,12 +87,12 @@ export default function Home() {
               className="hidden md:block w-[100px] absolute -top-10 -left-20"
               loading="lazy"
             />
-            <Image
+            {/* <Image
               src={mouseImage}
               alt="Mouse Image"
               className="hidden md:block w-[100px] absolute -bottom-10 -right-0"
               loading="lazy"
-            />
+            /> */}
           </div>
         </div>
       </div>
@@ -84,10 +100,19 @@ export default function Home() {
       {/* Featured Projects Section*/}
       <div className="relative -z-10 bg-[#1D1D1D] text-white">
         <div className="flex items-center justify-center">
-          <Image
-            src={pencilImage}
+          <motion.img
+            src={pencilImage.src}
             alt="Pencil Image"
-            className="absolute -top-24"
+            className="absolute -top-28"
+            animate={{
+              translateY: [-9, 9],
+            }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "mirror",
+              duration: 2,
+              ease: "easeInOut",
+            }}
           />
         </div>
         <div className="w-full uppercase flex flex-col items-center justify-center gap-y-6 py-32 div">
@@ -96,35 +121,70 @@ export default function Home() {
           </span>
           <h1 className="font-extrabold text-4xl md:text-5xl">Projects</h1>
         </div>
-        <div className="flex px-8 overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="relative flex gap-3 flex-none h-[510px] ">
-            {projects_data.map((project) => (
-              <div
-                key={project.altname}
-                className="w-full h-full border border-[#333333]"
-              >
-                <div className="relative w-full h-full">
-                  <Image
-                    src={project.imageSrc}
-                    alt={project.altname}
-                    className="w-full h-full"
-                  />
-                  <div className="absolute top-0 px-[20px] py-[47px] text-3xl font-extrabold">
-                    {project.num}
-                  </div>
-                  <div className="absolute top-44 w-[166px] font-semibold text-3xl px-[20px] py-[47px]">
-                    {project.jobTitle}
+        <div className="px-6">
+          <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+            <motion.div
+              className="relative flex gap-3 flex-none h-[510px] "
+              animate={{
+                translateX: "-50%",
+              }}
+              transition={{
+                duration: 20,
+                repeat: Infinity,
+                ease: "linear",
+                repeatType: "loop",
+              }}
+            >
+              {projects_data.map((project) => (
+                <div
+                  key={project.altname}
+                  className="w-full h-full border border-[#333333]"
+                >
+                  <div className="relative w-full h-full">
+                    <Image
+                      src={project.imageSrc}
+                      alt={project.altname}
+                      className="w-full h-full"
+                    />
+                    <div className="absolute inset-0 bg-black opacity-50"></div>
+                    <div className="absolute top-0 px-[20px] py-[47px] text-3xl font-extrabold">
+                      {project.num}
+                    </div>
+                    <div className="absolute top-44 w-[166px] font-semibold text-3xl px-[20px] py-[47px]">
+                      {project.jobTitle}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+              {projects_data.map((project) => (
+                <div
+                  key={project.altname}
+                  className="w-full h-full border border-[#333333]"
+                >
+                  <div className="relative w-full h-full">
+                    <Image
+                      src={project.imageSrc}
+                      alt={project.altname}
+                      className="w-full h-full"
+                    />
+                    <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+                    <div className="z-20 absolute top-0 px-[20px] py-[47px] text-3xl font-extrabold">
+                      {project.num}
+                    </div>
+                    <div className="z-20 absolute top-44 w-[166px] font-semibold text-3xl px-[20px] py-[47px]">
+                      {project.jobTitle}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </motion.div>
           </div>
         </div>
       </div>
       {/* Connect Section */}
       <Connect />
       {/* Footer */}
-      <Footer/>
+      <Footer />
     </section>
   );
 }
--- sections/Connect.tsx
+++ sections/Connect.tsx
@@ -9,7 +9,7 @@ type Props = {};
 const Connect = (props: Props) => {
   return (
     <div className="container">
-      <div className="py-4 w-full footer-bottom md:flex items-center justify-between">
+      <div className="py-12 w-full footer-bottom md:flex items-center justify-between md:py-4">
         <div className="flex flex-col gap-y-4 md:gap-y-10 md:w-1/2">
           <h1 className="text-4xl md:text-7xl font-black w-[121px]">
             Let's Create
