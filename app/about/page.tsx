c96086f7815bb71b36a03df51a36d92052f5299d
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-09-30 06:51:02 +0000

    Updates to about section padding

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,11 +9,11 @@ import React from "react";
 const About = () => {
   return (
     <section className="bg-about-img h-full scrollbar-hide">
-      <div className="px-8 pt-20 pb-16 w-full md:flex">
+      <div className="px-12 pt-20 pb-16 w-full md:flex md:px-16">
         <h1 className="text-[#BDBDBD] md:w-1/3 md:px-12 md:py-12 md:text-3xl">
           About Me
         </h1>
-        <p className="text-white mt-4 font-bold leading-loose md:mt-0 md:w-2/3 md:py-12 md:text-4xl md:leading-[3.2rem]">
+        <p className="text-white mt-4 font-medium leading-loose md:mt-0 md:w-2/3 md:py-12 md:text-4xl md:leading-[3.5rem]">
           Experienced Frontend Developer creating different full-stack solutions
           that are functional and visually pleasing with modern web
           technologies. I am eager to explore innovative approaches and grow my
@@ -21,7 +21,7 @@ const About = () => {
           opportunities.
         </p>
       </div>
-      <div className="px-8 pt-10 pb-20 w-full md:flex">
+      <div className="px-12 pt-10 pb-20 w-full md:px-16 md:flex">
         <h1 className="text-[#BDBDBD] md:w-1/3 md:px-12 md:py-12 md:text-3xl">
           Tech Stack
         </h1>
@@ -34,7 +34,7 @@ const About = () => {
               {tech}
             </div>
           ))}
-          <div className="py-4 text-base bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DDF,#DD7DDF)] text-transparent bg-clip-text md:text-6xl">
+          <div className="py-4 text-base bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DDF,#DD7DDF)] text-transparent bg-clip-text md:text-5xl">
             Always ready for a Tech Adventure
           </div>
         </div>
--- app/template.tsx
+++ app/template.tsx
@@ -11,40 +11,46 @@ type Props = {
 export default function Template({ children }: Props) {
   const pathname = usePathname();
   const [isLoading, setIsLoading] = useState(true); // State to manage loading animation
-  const [isVisible, setIsVisible] = useState(false); // State for child visibility
+  const [isVisible, setIsVisible] = useState(true); // State for child visibility
 
   // Function to handle when the animation completes
   const handleAnimationComplete = () => {
-    setTimeout(() => {
-      setIsLoading(false); // Hide loading animation after a short delay
-      setIsVisible(true); // Show children after loading is done
-    }, 300); // Adjust the delay time as needed
+    setIsLoading(false); // Hide loading animation
   };
+
+  // Update visibility when loading state changes
+  useEffect(() => {
+    if (!isLoading) {
+      setTimeout(() => {
+        setIsVisible(true); // Show children after loading is done
+      }, 300); // Adjust the delay time as needed
+    }
+  }, []);
 
   return (
     <div>
-      <AnimatePresence mode="wait" onExitComplete={() => setIsLoading(false)}>
+      <AnimatePresence mode="wait">
         {isLoading && (
           <motion.div key={pathname}>
             <motion.div
               className={`fixed top-0 left-0 right-0 bottom-0 z-30 h-screen w-full bg-[#111111] flex flex-col items-center justify-center text-center text-white`}
-              initial={{ y: "100%", opacity: 1 }} // Start off-screen to the right
-              animate={{ y: 0, opacity: 1 }} // Slide in from the right
-              exit={{ y: "-100%", opacity: 1 }} // Slide out to the left
+              initial={{ y: "100%", opacity: 1 }} // Start off-screen to the bottom
+              animate={{ y: 0, opacity: 1 }} // Slide in from the bottom
+              exit={{ y: "-100%", opacity: 1 }} // Slide out to the top
               transition={{
                 ease: "easeInOut",
                 duration: 0.75,
               }}
               onAnimationComplete={handleAnimationComplete} // Handle animation completion
             >
-              <div className="text-5xl capitalize">
-                {pathname}
-              </div>
+              <div className="text-5xl capitalize">{pathname}</div>
             </motion.div>
           </motion.div>
         )}
       </AnimatePresence>
-      {children}
+
+      {/* Ensure children are visible only after the loading is complete */}
+      {isVisible && !isLoading && children}
     </div>
   );
 }
