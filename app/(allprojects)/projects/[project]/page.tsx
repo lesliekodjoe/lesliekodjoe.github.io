f67ee3c0f816740a020e7675936e707efc7b7778
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-02 08:05:16 +0000

    Testing build

--- app/(allprojects)/projects/[project]/page.tsx
+++ app/(allprojects)/projects/[project]/page.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 const products = [
@@ -13,24 +12,26 @@ export async function generateStaticParams() {
   }));
 }
 
-interface ProjectProps{
-  params: string
+interface ProjectProps {
+  params: {
+    id: string;
+  };
 }
 
-const Project = ({params}: ProjectProps) => {
-    const id  = params;
-    const product = products.find((p) => p.id === id);
+const Project = ({ params }: ProjectProps) => {
+  const { id } = params;
+  const product = products.find((p) => p.id === id);
 
-    if (!product) {
-      return <div>Product not found</div>;
-    }
+  if (!product) {
+    return <div>Product not found</div>;
+  }
 
-    return (
-      <div>
-        <h1>{product.name}</h1>
-        <p>Product ID: {id}</p>
-      </div>
-    );
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>Product ID: {id}</p>
+    </div>
+  );
 };
 
 export default Project;
