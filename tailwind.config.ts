481addc2eec79cc3210a69c1a79d4307d5b86029
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-07 21:47:41 +0000

    Initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,30 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "10px",
+          lg: "80px",
+        },
       },
+      letterSpacing: {
+        "extra-wide": "0.5em",
+        "super-wide": "0.9em",
+      },
+      backgroundImage: {
+        "about-img": "url('../assets/background/about_background.jpg')",
+      },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
     },
   },
   plugins: [],
