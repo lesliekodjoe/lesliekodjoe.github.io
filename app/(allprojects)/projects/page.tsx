74e353e4a876a59013617aa280cc3a02b21a2985
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-12 23:16:21 +0000

    Skew approach - projects

--- app/(allprojects)/projects/page.tsx
+++ app/(allprojects)/projects/page.tsx
@@ -6,21 +6,73 @@ import proIMG1 from "@/assets/projectBackground/proIMG1.jpg";
 const ProjectList = () => {
   return (
     <section className="bg-about-img h-screen overflow-y-scroll overflow-x-hidden">
-      <div className="pt-32 pb-8 w-full h-full">
-        <div className="container w-[998px] flex flex-col items-center justify-center">
-          <div className="w-full h-[514px] -rotate-6">
-            <Image
-              src={proIMG1}
-              alt="image 1"
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="w-full h-[514px] -rotate-6">
-            <Image
-              src={proIMG1}
-              alt="image 1"
-              className="w-full h-full object-cover"
-            />
+      <div className="pt-32 pb-8 w-full h-full ">
+        <div className="border mx-auto flex flex-col items-center justify-center">
+          <div className="container flex flex-col items-center justify-center space-y-12 border -rotate-6">
+            {/* Stack the images vertically */}
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-[888px] h-[554px] ">
+              <Image
+                src={proIMG1}
+                alt="image 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
         </div>
       </div>
