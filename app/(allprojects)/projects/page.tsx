86619a6785932576752694e60185e6fca06dc5cb
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-10-16 13:19:51 +0000

    Update all projects animation

--- app/(allprojects)/projects/page.tsx
+++ app/(allprojects)/projects/page.tsx
@@ -7,20 +7,20 @@ import TransitionLink from "@/components/TransitionLink";
 const ProjectList = () => {
   return (
     <section className="bg-project-img w-full h-screen overflow-hidden">
-      <div className="pt-32 pb-8 w-full h-screen overflow-y-auto [mask-image:linear-gradient(to_bottom,transparent_10%,black,transparent_90%)]">
-        <div className="container flex flex-col items-center justify-center space-y-12 ">
+      <div className="pt-32 pb-8 w-full h-screen overflow-y-auto [mask-image:linear-gradient(to_top,transparent_5%,black_80%,transparent_95%)]">
+        <div className="container flex flex-col items-center justify-center space-y-8">
           {projects_data.map((project) => (
             <TransitionLink key={project.num} href={`/projects/${project.num}`}>
               <div className="group relative flex flex-col items-center justify-center">
-                <div className="absolute z-10 text-white">
-                  <h1 className="text-sm uppercase tracking-widest">
+                <div className="absolute z-20 text-white">
+                  <h1 className="text-sm uppercase tracking-widest font-medium">
                     {project.projectInfo.projectCategory}
                   </h1>
                   <h1 className="font-black text-5xl mt-4">
                     {project.projectInfo.projectName}
                   </h1>
                 </div>
-                <div className="w-full md:w-[888px] h-[504px]  opacity-60 ease-in-out duration-300 ">
+                <div className="w-full md:w-[888px] h-[504px] z-10 opacity-75 ease-in-out duration-300 ">
                   <Image
                     src={project.media.imageSrc}
                     alt={project.media.altname}
