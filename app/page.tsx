2fed0fa927771c7fca0bfa945deedfd1585cf45b
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-11-10 15:02:52 +0000

    Projects new design

--- app/page.tsx
+++ app/page.tsx
@@ -27,44 +27,6 @@ export default function Home() {
     <section className="">
       <Hero/>
       <Project/>
-      {/* <div className="pt-20 pb-36 lg:pb-32 md:pt-28 overflow-x-clip">
-        <div className="container">
-          <div className="-z-10 relative h-[280px] lg:h-[430px] flex justify-center items-center">
-            <div className="w-[309px] md:w-[788px] h-auto flex flex-col items-center justify-center">
-              <h1
-                ref={heroTitle}
-                className="hero-title text-center text-2xl font-bold text-design md:text-6xl md:leading-tight"
-              >
-                A Frontend Developer who loves designs & creating memorable user
-                experiences.
-              </h1>
-            </div>
-            <Image
-              src={coneImage}
-              alt="Cone Image"
-              className="w-[150px] lg:block lg:w-[230px] cone absolute top-0 -right-28"
-            />
-            <Image
-              src={donutImage}
-              alt="Donut Image"
-              className="w-[150px] -left-10 lg:block lg:w-[230px] donut absolute -bottom-28 lg:left-0"
-            />
-            <Image
-              src={watchImage}
-              alt="Watch Image"
-              className="watch hidden md:block w-[100px] absolute -top-10 -left-20"
-            />
-          </div>
-          <div className="flex w-full flex-row-reverse">
-            <TransitionLink
-              href="/about"
-              className="about-tag border border-black hover:bg-black hover:text-white"
-            >
-              About Me
-            </TransitionLink>
-          </div>
-        </div>
-      </div> */}
       {/* Featured Projects Section*/}
       {/* <div className="relative -z-10 bg-[#1D1D1D] text-white">
         <div className="flex items-center justify-center">
--- sections/landing/Project.tsx
+++ sections/landing/Project.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Project = () => {
+  const [activeTab, setActiveTab] = useState("work")
+
+  const handleTabButton = (tab:string) => {
+      setActiveTab(tab)
+  }
   return (
-    <div className='h-screen relative z-10 bg-[#1d1d1d] text-white'>
-        <div>
-            Projects
+    <div className="h-screen w-full relative z-10 bg-[#1d1d1d] text-white">
+      <div className="pt-20">
+        <div className="container bg-white bg-opacity-5 py-2 px-4 w-fit rounded-full border border-[#3f3f3f]">
+          <button
+            className={`w-[91px] h-[41px] ${
+              activeTab === "work"
+                ? "bg-[#3B3B3B] rounded-full font-bold"
+                : ""
+            }`}
+            onClick={()=> handleTabButton("work")}
+          >
+            Work
+          </button>
+          <button
+            className={`w-[91px] h-[41px] ${
+              activeTab === "design"
+                ? "bg-[#3B3B3B] rounded-full font-bold"
+                : ""
+            }`}
+            onClick={()=>handleTabButton("design")}
+          >
+            Design
+          </button>
         </div>
+        <div>
+          {
+            activeTab === "work" ? (
+              <div>Work Feed</div>
+            ) : (
+              <div>Design Feed</div>
+            )
+          }
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Project
