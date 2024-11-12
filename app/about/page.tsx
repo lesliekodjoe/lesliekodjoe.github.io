6202bb03c881687d50ed5e6b82a6cbbd525d5f3e
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2024-11-12 07:47:30 +0000

    Update about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import React, { useEffect } from "react";
 import Image from "next/image";
-import { hobbys_data, projects_data, tech_stack } from "@/utils/data";
+import {
+  design_data,
+  hobbys_data,
+  projects_data,
+  tech_stack,
+} from "@/utils/data";
 import TransitionLink from "@/components/TransitionLink";
 import Footer from "@/components/Footer";
 import Connect from "@/sections/Connect";
@@ -53,7 +58,7 @@ const About = () => {
             </div>
           </div>
           {/* Projects */}
-          <div className="py-16 px-6 md:px-0 mb-80">
+          <div className="py-16 px-6 md:px-0">
             <div className="project-anim-start "></div>
             <h1 className="about-header">Projects.</h1>
             <div className="mt-14 flex flex-col gap-y-10 md:flex-row">
@@ -82,37 +87,33 @@ const About = () => {
                 </TransitionLink>
               ))}
             </div>
-            {/* <div className="projects mt-14 relative w-full md:flex h-[363px] gap-x-5 items-center justify-center">
-              <TransitionLink
-                href="/projects"
-                className="absolute h-28 w-28 px-4 text-[14px] bg-white rounded-full uppercase hover:scale-110 hover:shadow-2xl ease-in-out duration-300"
-              >
-                Explore Projects
-              </TransitionLink>
-              <div className="w-full md:w-1/2 h-full">
-                <Image
-                  src={"/assets/projectBackground/proIMG1.jpg"}
-                  alt="image 1"
-                  width="0"
-                  height="0"
-                  sizes="100vw"
-                  className="w-full h-auto"
-                />
-              </div>
-              <div className="w-full md:w-1/2 h-full">
-                <Image
-                  src={"/assets/projectBackground/proIMG1.jpg"}
-                  alt="image 2"
-                  width="0"
-                  height="0"
-                  sizes="100vw"
-                  className="w-full h-auto"
-                />
-              </div>
-            </div> */}
+            <div className="mt-12">
+              <h1 className="about-header">Designs.</h1>
+              {design_data.map((design) => (
+                <div
+                  key={design.num}
+                  className="mt-14 w-full flex flex-col items-start group"
+                >
+                  <div className="w-full md:w-[480px] group-hover:scale-105 ease-in-out duration-500">
+                    <Image
+                      src={design.media.imageSrc}
+                      alt="projCard1"
+                      width="0"
+                      height="0"
+                      sizes="100vw"
+                      className="w-full h-full object-cover rounded-lg"
+                    />
+                  </div>
+                  <h4 className="mt-5 flex items-center">
+                    {design.designInfo.designName}
+                    <GoArrowUpRight className="text-xl ml-1 md:text-2xl transform group-hover:translate-x-2 group-hover:-translate-y-2 ease-in-out duration-500" />
+                  </h4>
+                </div>
+              ))}
+            </div>
           </div>
           {/* Fun & Hobbies */}
-          <div className="py-16 px-6 md:px-0 md:pt-40 hobby-start">
+          <div className="py-16 px-6 md:px-0 hobby-start">
             <h1 className="about-header">Other.</h1>
             <div className="hobbys-anim mt-14 md:flex gap-x-20 w-full ">
               {/* Left Column */}
