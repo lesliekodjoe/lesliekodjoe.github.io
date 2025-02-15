ba3098c16a5bc754eb9741b4139d184a5c1ddfc8
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2025-02-15 11:09:34 +0000

    Updating data

---
+++ src/constants/icons.ts
@@ -0,0 +1,18 @@
+import github  from "./icons/github.svg"
+import mail  from "./icons/mail.svg"
+import pencil  from "./icons/pencil.svg"
+import twitter  from "./icons/twitter.svg"
+import logoBlack  from "./icons/LogoBlack.svg"
+import logoWhite  from "./icons/LogoWhite.svg"
+
+
+
+
+export default {
+    github,
+    mail,
+    pencil,
+    twitter,
+    logoBlack,
+    logoWhite
+}
